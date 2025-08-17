315c941ccd2a1f52c52b4eda888bad0672cb0328
Chirayu <chirayuagrawal5@gmail.com>
2025-08-18 01:54:55 +0530

    Update MA DIFF calculator to remove the unused "open" array

--- src/algotrading_v40/feature_calculators/ma_diff.py
+++ src/algotrading_v40/feature_calculators/ma_diff.py
@@ -5,7 +5,6 @@ import algotrading_v40.utils.df as udf
 
 
 def ma_diff_(
-  open_: pd.Series,
   high: pd.Series,
   low: pd.Series,
   close: pd.Series,
@@ -13,13 +12,12 @@ def ma_diff_(
   long_length: int,
   lag: int,
 ) -> pd.Series:
-  for name, s in [("open", open_), ("high", high), ("low", low), ("close", close)]:
+  for name, s in [("high", high), ("low", low), ("close", close)]:
     if udf.analyse_numeric_series_quality(s).n_bad_values > 0:
       raise ValueError(f"{name} must not have bad values")
 
   return pd.Series(
     data=av40c_fc.ma_diff_cpp(
-      open_.values,
       high.values,
       low.values,
       close.values,
@@ -27,7 +25,7 @@ def ma_diff_(
       long_length,
       lag,
     ),
-    index=open_.index,
+    index=close.index,
   )
 
 
@@ -37,13 +35,11 @@ def ma_diff(
   long_length: int,
   lag: int,
 ) -> pd.DataFrame:
-  open_d = df["open"].shift(1)
   high_d = df["high"].shift(1)
   low_d = df["low"].shift(1)
   close_d = df["close"].shift(1)
 
   series = ma_diff_(
-    open_=open_d.iloc[1:],
     high=high_d.iloc[1:],
     low=low_d.iloc[1:],
     close=close_d.iloc[1:],
--- src/cpp/feature_calculators/ma_diff.cpp
+++ src/cpp/feature_calculators/ma_diff.cpp
@@ -8,22 +8,17 @@
 
 static pybind11::array_t<double> ma_diff_cpp(
     const pybind11::array_t<double, pybind11::array::c_style |
-                                        pybind11::array::forcecast> &open,
-    const pybind11::array_t<double, pybind11::array::c_style |
                                         pybind11::array::forcecast> &high,
     const pybind11::array_t<double, pybind11::array::c_style |
                                         pybind11::array::forcecast> &low,
     const pybind11::array_t<double, pybind11::array::c_style |
                                         pybind11::array::forcecast> &close,
     int short_length, int long_length, int lag) {
-
-  const auto [open_, n1] = atv40::io::get_input_ptr<double>(open, "open");
-  const auto [high_, n2] = atv40::io::get_input_ptr<double>(high, "high");
-  const auto [low_, n3] = atv40::io::get_input_ptr<double>(low, "low");
-  const auto [close_, n4] = atv40::io::get_input_ptr<double>(close, "close");
-  if (n1 != n2 || n1 != n3 || n1 != n4) {
-    throw std::runtime_error(
-        "open, close, high, and low must have the same length");
+  const auto [high_, n1] = atv40::io::get_input_ptr<double>(high, "high");
+  const auto [low_, n2] = atv40::io::get_input_ptr<double>(low, "low");
+  const auto [close_, n3] = atv40::io::get_input_ptr<double>(close, "close");
+  if (n1 != n2 || n1 != n3) {
+    throw std::runtime_error("high, low, and close must have the same length");
   }
   const int n = n1;
   auto [out, output] = atv40::io::make_output_array<double>(n);
@@ -72,8 +67,8 @@ static pybind11::array_t<double> ma_diff_cpp(
 }
 
 void register_ma_diff(pybind11::module_ &m) {
-  m.def("ma_diff_cpp", &ma_diff_cpp, pybind11::arg("open"),
-        pybind11::arg("high"), pybind11::arg("low"), pybind11::arg("close"),
+  m.def("ma_diff_cpp", &ma_diff_cpp, pybind11::arg("high"),
+        pybind11::arg("low"), pybind11::arg("close"),
         pybind11::arg("short_length"), pybind11::arg("long_length"),
         pybind11::arg("lag"),
         "Compute the Moving-Average Difference indicator");
