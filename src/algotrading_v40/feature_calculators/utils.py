27db42fa4480e5170bce3bc335fde63c1f70fb4b
Chirayu <chirayuagrawal5@gmail.com>
2025-09-28 21:57:46 +0530

    Add feature calculation functions for guaranteed last value (calculate_features_with_last_value_guaranteed) and minimum data points needed for stable good value (find_min_data_points_needed_for_stable_good_last_value). Update utils.py with new functions and enhance tests in utils_test.py. Modify feature_calculators.ipynb to demonstrate new functionality.

--- src/algotrading_v40/feature_calculators/utils.py
+++ src/algotrading_v40/feature_calculators/utils.py
@@ -7,6 +7,45 @@ import algotrading_v40.bar_groupers.time_based_uniform as bg_tbu
 import algotrading_v40.utils.df as u_df
 
 
+def calculate_features_with_last_value_guaranteed(
+  df: pd.DataFrame,
+  f_calc: Callable[[pd.DataFrame], pd.DataFrame],
+  group_size_minutes: int,
+):
+  """
+  Calculate features with the last value guaranteed.
+
+  This function trades off speed for coverage. Apart from the last value, the other values can be nans.
+  It will be useful for cases when only the last value is needed.
+  Example: Streaming, find_min_data_points_needed_for_stable_good_last_value
+
+  For complete coverage, use calculate_features_with_complete_coverage.
+  """
+  # calculate last value's offset
+  tbubg_result_ = bg_tbu.get_time_based_uniform_bar_group_for_indian_market(
+    df=df,
+    group_size_minutes=group_size_minutes,
+    offset_minutes=0,
+  )
+  last_offset = tbubg_result_.offsets.values[-1]
+
+  # pass offset as last value's offset
+  # this ensure last will be the first bar of the resampled df
+  # => value will be calculated for it
+  tbubg_result = bg_tbu.get_time_based_uniform_bar_group_for_indian_market(
+    df=df,
+    group_size_minutes=group_size_minutes,
+    offset_minutes=last_offset,
+  )
+  with pd.option_context("mode.chained_assignment", None):
+    df["bar_group"] = tbubg_result.bar_groups
+    df["offset"] = tbubg_result.offsets
+    result = u_df.calculate_grouped_values(df=df, compute_func=f_calc)
+    df.drop(columns=["bar_group", "offset"], inplace=True)
+
+  return result
+
+
 def calculate_features_with_complete_coverage(
   df: pd.DataFrame,
   f_calc: Callable[[pd.DataFrame], pd.DataFrame],
@@ -31,11 +70,12 @@ def calculate_features_with_complete_coverage(
       group_size_minutes=group_size_minutes,
       offset_minutes=offset_minutes,
     )
-    df["bar_group"] = tbubg_result.bar_groups
-    df["offset"] = tbubg_result.offsets
-    results.append(u_df.calculate_grouped_values(df=df, compute_func=f_calc))
-    all_offsets.append(tbubg_result.offsets.values)
-    df.drop(columns=["bar_group", "offset"], inplace=True)
+    with pd.option_context("mode.chained_assignment", None):
+      df["bar_group"] = tbubg_result.bar_groups
+      df["offset"] = tbubg_result.offsets
+      results.append(u_df.calculate_grouped_values(df=df, compute_func=f_calc))
+      all_offsets.append(tbubg_result.offsets.values)
+      df.drop(columns=["bar_group", "offset"], inplace=True)
 
   # Stack all offset arrays into a 2D array (rows=offset_minutes sent as input , cols=offsets returned as output)
   offsets_array = np.stack(all_offsets, axis=0)
@@ -74,3 +114,51 @@ def calculate_features_with_complete_coverage(
       output_df.loc[:first_false_from_end, col] = np.nan
 
   return output_df
+
+
+def find_min_data_points_needed_for_stable_good_last_value(
+  df: pd.DataFrame,
+  f_calc: Callable[[pd.DataFrame], pd.DataFrame],
+) -> int:
+  """
+  Find the minimum number of data points needed for a stable good value for the last row.
+
+  For certain features (e.g., those involving an exponential moving average), calculating a value with (say) 10 rows
+  can differ from calculating a value using (say) 100 rows (for the same timestamp). Values can be good (i.e. non NaN, inf, None etc.) in both cases
+  but not match for the same timestamp.
+
+  This function uses binary search to find the minimum number of data points needed such that the last value is a stable good value.
+
+  Concrete example:
+  say calling feature_A() on a dataframe df with 100 rows yields first 5 values bad (NaN, inf, None etc.) and last 95 values good (6th is the first good value).
+  it is possible that some of the initial seemingly good looking values out of all 95 are bad.
+  i.e. if we call feature_A() on df.iloc[1:] and reindex the result to df.index, ideally we should get first 6 values bad, all good values from 7th onwards
+  AND the 7th value to match the 7th value of the original result dataframe.
+  In case of some features, it is possible that this is not the case. So we should discard the 7th value as well in the original result dataframe.
+  But how many values to discard?
+  This function can be used to answer this question.
+  It binary searches for the smallest number of datapoints that are needed for a stable last value.
+
+  So if this function returns 12 for feature_A(), we should discard the first 11 values from the original result dataframe (set them to NaN) since
+  the 12th value is the first stable good value.
+
+  NOTE: For the same feature_A, this function can return different results for different slices of a df since it depends on the actual values of the dataframe.
+  However, the values should be close to each other and not differ wildly (i.e. getting 100 for one slice and 1200 for another slice (of the same df and for the same feature_A)
+  is not expected).
+  If you want to be extra cautious, use a safety factor to increase the number of values to discard (i.e. safety factor of 1.05 means discard 105 values if this function returns 100).
+
+  """
+  last_row = f_calc(df).iloc[-1]
+  if not np.isfinite(last_row).all():
+    raise ValueError("Last row should have all finite values")
+  if np.isclose(f_calc(df.iloc[(len(df) - 1) :]).iloc[-1], last_row).all():
+    return 1
+  low, high = 0, len(df) - 1
+  # value matches at low and does not match at high
+  while low < high - 1:
+    mid = (low + high) // 2
+    if np.isclose(f_calc(df.iloc[mid:]).iloc[-1], last_row).all():
+      low = mid
+    else:
+      high = mid
+  return len(df) - low
--- src/algotrading_v40/feature_calculators/utils_test.py
+++ src/algotrading_v40/feature_calculators/utils_test.py
@@ -1,9 +1,12 @@
+import datetime as dt
+
 import numpy as np
 import pandas as pd
 
+import algotrading_v40.feature_calculators.detrended_rsi as fc_drsi
 import algotrading_v40.feature_calculators.utils as fc_utils
 import algotrading_v40.utils.df as u_df
-import algotrading_v40.utils.streaming as us
+import algotrading_v40.utils.streaming as u_s
 import algotrading_v40.utils.testing as u_t
 
 
@@ -164,7 +167,7 @@ class TestCalculateFeaturesWithCompleteCoverage:
 
   #   pd.testing.assert_frame_equal(result_df, result_df_expected)
 
-  #   bsr = us.compare_batch_and_stream(
+  #   bsr = u_s.compare_batch_and_stream(
   #     df,
   #     lambda df_: fc_utils.calculate_features_with_complete_coverage(
   #       df=df_,
@@ -353,7 +356,7 @@ class TestCalculateFeaturesWithCompleteCoverage:
 
     df = _get_test_df()
     group_size_minutes = 3
-    bsr = us.compare_batch_and_stream(
+    bsr = u_s.compare_batch_and_stream(
       df,
       lambda df_: fc_utils.calculate_features_with_complete_coverage(
         df=df_,
@@ -362,11 +365,12 @@ class TestCalculateFeaturesWithCompleteCoverage:
       ),
     )
     assert bsr.dfs_match
-
+    # Although streaming matches batch in this test case,
     # STREAMING CAN MISMATCH BATCH IN CASES LIKE nan,2,nan,3,4,5
     # IN THIS CASE STREAMING WILL GIVE nan,2,nan,3,4,5
     # BATCH WILL GIVE nan,nan,nan,3,4,5
     # THIS WILL HAPPEN RARELY SO WE ALLOW IT.
+    # See test_results_have_bad_values_at_start_only which shows a case where streaming mismatches batch
 
   def test_results_have_bad_values_at_start_only(self):
     def _f_calc_with_initial_nans_3(df: pd.DataFrame):
@@ -417,11 +421,13 @@ class TestCalculateFeaturesWithCompleteCoverage:
       result_df = fc_utils.calculate_features_with_complete_coverage(
         df=df, f_calc=_f_calc_with_initial_nans_3, group_size_minutes=3
       )
+
+    # check that bad values are at the start only
     qual = u_df.analyse_numeric_columns_quality(df=result_df)
     for col, qual in qual.items():
       assert qual.n_bad_values == qual.n_bad_values_at_start
 
-    bsr = us.compare_batch_and_stream(
+    bsr = u_s.compare_batch_and_stream(
       df,
       lambda df_: fc_utils.calculate_features_with_complete_coverage(
         df=df_,
@@ -431,3 +437,169 @@ class TestCalculateFeaturesWithCompleteCoverage:
     )
     # THIS TEST CASE IS ENGINEERED TO BE STREAM UNSAFE
     assert not bsr.dfs_match
+
+
+class TestCalculateFeaturesWithLastValueGuaranteed:
+  def test_last_value_matches_complete_coverage(self):
+    df = _get_test_df()
+    group_size_minutes = 3
+
+    def _f_calc(df_: pd.DataFrame):
+      dfr = pd.DataFrame(index=df_.index, columns=["close_squared"])
+      dfr["close_squared"] = df_["close"].pow(2)
+      return dfr
+
+    with u_t.expect_no_mutation(df):
+      res_last = fc_utils.calculate_features_with_last_value_guaranteed(
+        df=df, f_calc=_f_calc, group_size_minutes=group_size_minutes
+      )
+      res_full = fc_utils.calculate_features_with_complete_coverage(
+        df=df, f_calc=_f_calc, group_size_minutes=group_size_minutes
+      )
+
+    # res_full has all values
+    assert np.isfinite(res_full.values).all()
+    # res_last should not have all values when this test data is used
+    assert not np.isfinite(res_last.values).all()
+    # Verify the last row values match and are finite.
+    pd.testing.assert_series_equal(res_last.iloc[-1], res_full.iloc[-1])
+    assert np.isfinite(res_last.iloc[-1]).all()
+
+  def test_streaming_lvg_matches_batch_cc(self):
+    # while strategy development, we will calculate features with calculate_features_with_complete_coverage.
+    # but that function is too slow for streaming (i.e. while trading live).
+    # so for that, we will use calculate_features_with_last_value_guaranteed.
+    # this function guarantees that just the last value is a good value.
+    # this test checks that creating a result by calculating one by one using calculate_features_with_last_value_guaranteed
+    # matches the result created by calculating all at once using calculate_features_with_complete_coverage.
+    # this is necessary to ensure that feature values during strategy development and live trading are the same.
+
+    # we cannot use u_s.compare_batch_and_stream here as we want to compare
+    # results from different functions.
+    # so we are using DfStreamer directly to test that streaming last_value_guaranteed (lvg)
+    # matches batch complete_coverage (cc).
+    def f_calc_lvg(df):
+      # faster as only the values needed to compute the last value are calculated
+      return fc_utils.calculate_features_with_last_value_guaranteed(
+        df=df,
+        f_calc=lambda df: fc_drsi.detrended_rsi(
+          df=df, short_length=2, long_length=20, length=30
+        ),
+        group_size_minutes=5,
+      )
+
+    def f_calc_cc(df):
+      # slower as all values are calculated
+      return fc_utils.calculate_features_with_complete_coverage(
+        df=df,
+        f_calc=lambda df: fc_drsi.detrended_rsi(
+          df=df, short_length=2, long_length=20, length=30
+        ),
+        group_size_minutes=5,
+      )
+
+    np.random.seed(42)
+    df = u_t.get_test_df(start_date=dt.date(2023, 1, 1), end_date=dt.date(2023, 1, 2))
+    streamer = u_s.DfStreamer(df)
+    result_batch = f_calc_cc(df)
+
+    # just check that there are enough good values to make the test meaningful
+    assert (
+      u_df.analyse_numeric_series_quality(
+        result_batch["detrended_rsi_2_20_30"]
+      ).n_good_values
+      > 100
+    )
+    for i in range(len(df)):
+      df_ = streamer.next()
+      result_stream = f_calc_lvg(df_)
+      assert result_batch.iloc[i].equals(result_stream.iloc[-1])
+
+
+class TestFindMinDataPointsNeededForStableGoodLastValue:
+  def test_direct_dependency_returns_one(self):
+    df = _get_test_df()
+
+    def _f_calc(df_: pd.DataFrame):
+      return pd.DataFrame({"close": df_["close"]})
+
+    assert (
+      fc_utils.find_min_data_points_needed_for_stable_good_last_value(
+        df=df, f_calc=_f_calc
+      )
+      == 1
+    )
+
+  def test_lagged_rolling_three_requires_four_rows(self):
+    df = _get_test_df()
+
+    def _f_calc(df_: pd.DataFrame):
+      return pd.DataFrame({"close_roll3": df_["close"].shift(1).rolling(3).mean()})
+
+    assert (
+      fc_utils.find_min_data_points_needed_for_stable_good_last_value(
+        df=df, f_calc=_f_calc
+      )
+      == 4
+    )
+
+  def test_an_exponential_moving_average_feature(self):
+    np.random.seed(42)
+    df = u_t.get_test_df(start_date=dt.date(2023, 1, 1), end_date=dt.date(2023, 2, 1))
+    # just checking that the test data is large enough
+    assert len(df) > 1000
+
+    def f_calc_lvg(df):
+      # faster as only the values needed to compute the last value are calculated
+      return fc_utils.calculate_features_with_last_value_guaranteed(
+        df=df,
+        f_calc=lambda df: fc_drsi.detrended_rsi(
+          df=df, short_length=2, long_length=20, length=252
+        ),
+        group_size_minutes=5,
+      )
+
+    def f_calc_cc(df):
+      # slower as all values are calculated
+      return fc_utils.calculate_features_with_complete_coverage(
+        df=df,
+        f_calc=lambda df: fc_drsi.detrended_rsi(
+          df=df, short_length=2, long_length=20, length=252
+        ),
+        group_size_minutes=5,
+      )
+
+    min1 = fc_utils.find_min_data_points_needed_for_stable_good_last_value(
+      df=df, f_calc=f_calc_lvg
+    )
+
+    # just checking that the test data is such that min1 is substantial
+    assert min1 > 1000
+
+    # for this usecase, using the faster version should make no difference as only the last value is needed
+    # this assert verifies that
+    assert min1 == fc_utils.find_min_data_points_needed_for_stable_good_last_value(
+      df=df, f_calc=f_calc_cc
+    )
+
+    val_partial = f_calc_lvg(df.iloc[-min1:])["detrended_rsi_2_20_252"].values[-1]
+
+    # just checking that the test data is such that the last value is finite and comfortably above 0
+    assert np.isfinite(val_partial)
+    assert np.abs(val_partial) > 1
+
+    # this assert verifies that the min rows needed are correct
+    # i.e. the last value computed using just them is the same as the last value computed using all rows
+    assert np.isclose(
+      val_partial,
+      f_calc_lvg(df)["detrended_rsi_2_20_252"].values[-1],
+    )
+
+    # this assert verifies that the min1 is indeed the min rows needed
+    # it computes the last value using min1-1 rows and verifies that it differs from the last value computed using all rows
+    # => min1-1 is not sufficient to match the last value computed using all rows
+    # => min1 is indeed the min
+    assert not np.isclose(
+      f_calc_lvg(df.iloc[-(min1 - 1) :])["detrended_rsi_2_20_252"].values[-1],
+      f_calc_lvg(df)["detrended_rsi_2_20_252"].values[-1],
+    )
